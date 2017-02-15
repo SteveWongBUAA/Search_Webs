06b60d7a9657e91a509153b7b411751554e09a83
stevewong <413981956@qq.com>
2017-02-15 21:47:48 +0800

    hush

--- Request.py
+++ Request.py
@@ -8,6 +8,7 @@ import ExtConfigParser
 import traceback
 from bs4 import BeautifulSoup
 import sys
+import json
 
 
 class Request(object):
@@ -28,8 +29,9 @@ class Request(object):
     def getConf(self, key):
         cf = ExtConfigParser.ExtConfigParser()
         cf.read("./conf/urls.conf")
-        self.data = cf.getDict(['url', 'res_kw', 'method', 'should_have_key', 'hdr_more', 'encoding', 'post_key'])
-        # print self.data
+        self.data = cf.getDict(['url', 'res_kw', 'method', 'should_have_key', 'hdr_more', 'encoding',
+                                'post_key', 'more_formdata', 'must_fully_match'])
+        # print self.data[12]
         for id in self.data.keys():
             if type(key) == unicode:
                 key_encode = key.encode(encoding=self.data[id]['encoding'])
@@ -47,6 +49,9 @@ class Request(object):
                 url = self.data[id]['url']
             else:
                 continue
+
+            if self.data[id].has_key('more_formdata'):
+                self.data[id]['more_formdata'] = json.loads(self.data[id]['more_formdata'])
 
             self.data[id]['url'] = url
             self.data[id]['key_quote'] = key_quote
@@ -85,56 +90,96 @@ class Request(object):
             hdr = self.hdr
 
         if self.data[id]['method'] == 'post':
-            post_data = urllib.urlencode({self.data[id]['post_key']: self.raw_key})
+            post_data = {self.data[id]['post_key']: self.raw_key}
+            if self.data[id].has_key('more_formdata'):
+                # merge
+                post_data = dict(self.data[id]['more_formdata'], **post_data)
+            post_data = urllib.urlencode(post_data)
+
+
             req = urllib2.Request(self.data[id]['url'], headers=hdr, data=post_data)
         else:
             req = urllib2.Request(self.data[id]['url'], headers=hdr)
 
         try:
-            # page = urllib2.urlopen(req)
-            # f = page.read()
-            # bs = BeautifulSoup(f)
-            # return str(bs)
-            req.post
+            page = urllib2.urlopen(req, timeout=5)
+            f = page.read()
+            bs = BeautifulSoup(f)
+            # webbrowser.open(page.geturl())
+            return page.geturl(), str(bs)
+
         except:
-            print traceback.format_exc()
+            print id, traceback.format_exc()
             return None
 
         # input key is unicode should encode
-
         #     has_res = self.has_res(url, hdr_more=one['hdr_more'], should_have_key=one['should_have_key'], key=one['key'], )
         #     if has_res:
         #         webbrowser.open(url)
         #         urls.append(url)
         # return urls
 
-    def has_res1(self, id):
-        f = self.request1(id)
+    def check1(self, id):
+        """
+        check 1 url, if ok, return the final url(maybe redirected), else return None
+        :param id:
+        :return:
+        """
+        res_url, f = self.request1(id)
+        print id, 'res_url', res_url
+        # print f
         if f is None:
-            return False
-        else:
-            findres = f.find(self.data[id]['res_kw'])
-            if self.data[id]['should_have_key'] == 'y':
-                # 应该有key而且真的找到了key
-                if findres != -1:
-                    return True
-                else:
-                    return False
-            else:
-                # 不应该有key但是找到了key
-                if findres != -1:
-                    return False
-                else:
-                    return True
+            print id, '# fail f is none'
+            return None
+        # 如果这个网站需要完全匹配关键字 但是 并不是完全匹配(没找到)(还要过滤掉第一个在搜索框里匹配的)，就返回None
+        if self.data[id]['must_fully_match'] == 'y' and f.find(self.raw_key, f.find(self.raw_key)+1) == -1:
+            print id, '# fail must fully match but no'
+            return None
 
+        findres = f.find(self.data[id]['res_kw'])
+        if self.data[id]['should_have_key'] == 'y':
+            # 应该有key而且真的找到了key
+            if findres != -1:
+                return res_url
+            else:
+                print id, '# fail should have key but no'
+                return None
+        else:
+            # 不应该有key但是找到了key
+            if findres != -1:
+                print id, '# fail should not have key but yes'
+                return None
+            else:
+                return res_url
+
+
+    def test_reqs(self, id, post_data):
+        import requests
+        r = requests.post(self.data[id]['url'], post_data)
+        print r.history
+        print r.headers
+        print r.is_redirect
+        print r.links
+        print r.text
 
 
 if __name__ == '__main__':
     # Search_Webs().search("homeland")
     # Search_Webs().search("xzchkjq")
     # Search_Webs().search("你的名字")
-    rqt = Request('homeland')
+    # rqt = Request('你的名字')
+    rqt = Request('你的名字')
     # print rqt.data[12]
-    print rqt.request1(15)
+    for i in range(0, 20):
+        try:
+            data = rqt.data[i]
+            print i, data['url']
+            res = rqt.check1(i)
+            print res
+            if res is not None:
+                # print 'res', res
+                webbrowser.open(res)
+        except:
+            print traceback.format_exc()
 
 
--- Thread_Search.py
+++ Thread_Search.py
@@ -2,70 +2,65 @@
 # -*- coding: utf-8 -*-
 
 import time, threading
-from Request import Search_Webs
+from Request import Request
 import urllib, urllib2
 from bs4 import BeautifulSoup
 import traceback
 import sys
-
+import webbrowser
 
 class Thread_Search(object):
-    def __init__(self):
-        self.sw = Search_Webs()
+    def __init__(self, key):
+        self.sw = Request(key)
         self.data = self.sw.data
         self.hdr = self.sw.hdr
         self.res_urls = []
 
-    def request(self, url, hdr_more, key, should_have_key):
+    def request(self, id):
+        url = self.sw.data[id]['url']
         print('### thread %s is running... url = %s' % (threading.current_thread().name, url))
-        hdr = self.hdr
-        if hdr_more == 'y':
-            hdr['X-Requested-With'] = 'XMLHttpRequest'
 
-        req = urllib2.Request(url, headers=hdr)
-        try:
-            page = urllib2.urlopen(req)
-            f = page.read()
-            f = str(BeautifulSoup(f))
+        res = self.sw.check1(id)
+        if res is not None:
+            webbrowser.open(res)
+            self.res_urls.append(res)
 
-        except:
-            print('### thread %s failed...\ntraceback: %s' % (threading.current_thread().name, str(traceback.format_exc())))
-            # self.ret_content[url] = None
-            return
-
-
-        # print key, f
-        findres = f.find(key)
-        # print 'findres', findres
-        if should_have_key == 'y' and findres != -1:
-            # 应该有key而且真的找到了key
-            self.res_urls.append(url)
-        if should_have_key == 'n' and findres == -1:
-            # 不应该有key而且没有找到了key
-            self.res_urls.append(url)
+        # hdr = self.hdr
+        # if hdr_more == 'y':
+        #     hdr['X-Requested-With'] = 'XMLHttpRequest'
+        #
+        # req = urllib2.Request(url, headers=hdr)
+        # try:
+        #     page = urllib2.urlopen(req)
+        #     print('### thread %s url open end... url = %s' % (threading.current_thread().name, url))
+        #     f = page.read()
+        #     f = str(BeautifulSoup(f))
+        #
+        # except:
+        #     print('### thread %s failed...\ntraceback: %s' % (threading.current_thread().name, str(traceback.format_exc())))
+        #     # self.ret_content[url] = None
+        #     return
+        #
+        #
+        # # print key, f
+        # findres = f.find(key)
+        # # print 'findres', findres
+        # if should_have_key == 'y' and findres != -1:
+        #     # 应该有key而且真的找到了key
+        #     self.res_urls.append(url)
+        # if should_have_key == 'n' and findres == -1:
+        #     # 不应该有key而且没有找到了key
+        #     self.res_urls.append(url)
 
         print('### thread %s ended.' % threading.current_thread().name)
 
 
 
 
-    def search(self, key):
+    def search(self, num):
         thread_pool = []
-        for id in self.data:
-            if type(key) == unicode:
-                key_encode = key.encode(encoding=id['encoding'])
-            elif type(key) == str:
-                key_encode = key.decode(sys.stdin.encoding).encode(id['encoding'])
-            else:
-                continue
-            key_quote = urllib.quote(key_encode)
-            if id['method'] == 'concat':
-                url = id['url'].replace('{#concat#}', key_quote)
-            elif id['method'] == 'append':
-                url = id['url'] + key_quote
-            else:
-                continue
-            nowthread = threading.Thread(target=self.request, args=(url, id['hdr_more'], id['key'], id['should_have_key']))
+        for id in range(num):
+            nowthread = threading.Thread(target=self.request, args=(id, ))
             thread_pool.append(nowthread)
             nowthread.start()
         for nowthread in thread_pool:
@@ -74,6 +69,6 @@ class Thread_Search(object):
 
 
 if __name__ == '__main__':
-    Thread_Search().search('homeland')
+    Thread_Search('homeland').search(20)
 
 
